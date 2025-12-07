6a43d9f2789ed9d065ad073e6d841f56db4c661c
tirachrach950-sys <tirachrach950@gmail.com>
2025-12-07 18:25:53 +0100

    Update _app.tsx

--- alx-project-0x07/ pages/_app.tsx
+++ alx-project-0x07/ pages/_app.tsx
@@ -1,11 +1,8 @@
-import Layout from "@/components/layouts/Layout";
-import "@/styles/globals.css";
+// pages/_app.tsx
+
+import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
