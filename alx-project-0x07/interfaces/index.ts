98325c9e007191664e32c42e64223168ae8cfe63
tirachrach950-sys <tirachrach950@gmail.com>
2025-12-07 18:44:33 +0100

    Update index.ts

--- alx-project-0x07/interfaces/index.ts
+++ alx-project-0x07/interfaces/index.ts
@@ -1,1 +1,16 @@
+// interfaces/index.ts
+
+import * as React from "react";
+
+// Generic interface for components that receive children
+export interface ReactComponentProps {
+  children: React.ReactNode;
+}
+
+// Example: you can also define other interfaces here if needed
+export interface ExampleInterface {
+  id: number;
+  name: string;
+  description?: string;
+}
 
